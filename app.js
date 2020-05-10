b46098dc5dc51b256ef55e6fab65983c7326c412
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-05-10 03:30:15 -0700

    2:44:38

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-var userInput;
-var userName;
-userInput = 5;
-if (typeof userInput === "string") {
-    userName = userInput;
-}
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-generateError("an error occuered!", 500);
+"use strict";
+var userName = "max";
+console.log(userName);
--- app.ts
+++ app.ts
@@ -1,14 +1,3 @@
-let userInput: unknown;
-let userName: string;
+const userName = "max";
 
-userInput = 5;
-
-if (typeof userInput === "string") {
-  userName = userInput;
-}
-
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-}
-
-generateError("an error occuered!", 500);
+console.log(userName);
